f23c4e4efdfaaaea8530b73b8977fffcc6128d5c
veenadondapati <dondapativeena@gmail.com>
2021-05-10 14:45:04 +0100

    Initial Commit

---
+++ src/main/java/com/codeboard/coadeboardaws/CoadeboardAwsApplication.java
@@ -0,0 +1,13 @@
+package com.codeboard.coadeboardaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoadeboardAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoadeboardAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeboard/coadeboardaws/CoadeboardAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeboard.coadeboardaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoadeboardAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
